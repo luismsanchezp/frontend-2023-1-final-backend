6615bb79aec5d033618cbdd38ab5757487996001
luismsanchezp <luism.sanchezp@autonoma.edu.co>
2023-05-29 08:06:29 -0500

    Fix vulnerability where inactive user gets accessToken

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -6,14 +6,18 @@ async function signIn(req, res) {
     const email = req.body.email.toLowerCase();
     const password = req.body.password;
     
-    User.findOne({ email: email }, 'password role')
+    User.findOne({ email: email }, 'password role active')
     .then(async (user) => {
         console.debug(user);
         comparePassword(password, user.password)
         .then(async (match) => {
             if (match) {
-                const accessToken = await createAccessToken(user);
-                res.status(200).send({ accessToken });
+                if (user.active) {
+                    const token = await createAccessToken(user);
+                    res.status(200).send({ accessToken: token });
+                } else {
+                    res.status(401).send({ error: {active: false} });
+                }
             } else {
                 res.status(401).send({ error: "Password does not match." });
             }
